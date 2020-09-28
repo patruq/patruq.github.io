a32d9e5835d16c8effeb54ca4b007e1fb09e0c4a
patruq <pattymac.pm@gmail.com>
2020-09-28 15:43:24 -0400

    added project content

--- personal-website/src/components/routes/portfolio.js
+++ personal-website/src/components/routes/portfolio.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const GridWrapper = styled.div`
-    display: grid;
-    grid-gap: 10px;
-    margin-top: 1em;
-    margin-left: 6em;
-    margin-right: 6em;
-    grid-template-columns: repeat(12, 1fr);
-    grid-auto-rows: minmax(25px, auto);
-`;
+import '../../assets/css/portfolio.css'
 
 export const Portfolio= (props) => (
-    <GridWrapper>
-        <p>This is the PORTFOLIO page</p>
-    </GridWrapper>
+    <body>
+        <div className="top">
+            <h2>Projects</h2>
+        </div>
+        <div className="content">
+            <div className="upper-left">
+                <img className="upper-left-img" src={require("../../assets/images/wildfire.webp")} />
+            </div>
+            <div className="upper-center">
+                <img className="upper-center-img" src={require("../../assets/images/city.jpg")} />
+            </div>
+            <div className="upper-right">
+                <img className="upper-right-img" src={require("../../assets/images/social-media.webp")} />
+            </div>
+            <div className="bottom-left">
+                <img className="bottom-left-img" src={require("../../assets/images/house.webp")} />
+            </div>
+            <div className="bottom-right">
+                <img className="bottom-right-img" src={require("../../assets/images/school.webp")} />
+            </div>
+        </div>
+    </body>
 )
