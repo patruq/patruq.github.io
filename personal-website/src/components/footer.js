d4a75a355243a3a9d9573013c5ee0a1263513298
patruq <pattymac.pm@gmail.com>
2020-09-28 17:06:46 -0400

    removed icons from footer

--- personal-website/src/components/footer.js
+++ personal-website/src/components/footer.js
@@ -5,12 +5,12 @@ export const Footer = () => (
     // <Styles>
     <body>
         <div className="main-footer">
-            <div className="icons">
-            <h4>Icons</h4>
+            {/* <div className="icons"> */}
+            {/* <h4>Icons</h4> */}
                 <div className="copyright">
                     <h6>Copyright Info</h6>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     {/* </Styles> */}
     </body>
