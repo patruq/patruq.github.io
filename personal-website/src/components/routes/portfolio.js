ab87c83f38b8b29c5950e9d5b3e3bdb727560fe8
patruq <pattymac.pm@gmail.com>
2020-09-28 16:57:50 -0400

    added links to project images

--- personal-website/src/components/routes/portfolio.js
+++ personal-website/src/components/routes/portfolio.js
@@ -9,19 +9,29 @@ export const Portfolio= (props) => (
         </div>
         <div className="content">
             <div className="upper-left">
-                <img className="upper-left-img" src={require("../../assets/images/wildfire.webp")} />
+                <a href="https://github.com/patruq/Mapping-Disaster-Tweets-CACampFire" target="blank">
+                    <img className="upper-left-img" src={require("../../assets/images/wildfire.webp")} />
+                </a>
             </div>
             <div className="upper-center">
-                <img className="upper-center-img" src={require("../../assets/images/city.jpg")} />
+                <a href="https://github.com/patruq/urban-at-risk" target="blank">
+                    <img className="upper-center-img" src={require("../../assets/images/city.jpg")} />
+                </a>
             </div>
             <div className="upper-right">
-                <img className="upper-right-img" src={require("../../assets/images/social-media.webp")} />
+                <a href="https://github.com/patruq/reddit-nlp" target="blank">
+                    <img className="upper-right-img" src={require("../../assets/images/social-media.webp")} />
+                </a>
             </div>
             <div className="bottom-left">
-                <img className="bottom-left-img" src={require("../../assets/images/house.webp")} />
+                <a href="https://github.com/patruq/ames-housing" target="blank">
+                    <img className="bottom-left-img" src={require("../../assets/images/house.webp")} />
+                </a>
             </div>
             <div className="bottom-right">
-                <img className="bottom-right-img" src={require("../../assets/images/school.webp")} />
+                <a href="https://github.com/patruq/act-sat-performance" target="blank">
+                    <img className="bottom-right-img" src={require("../../assets/images/school.webp")} />
+                </a>
             </div>
         </div>
         {/* <Footer /> */}
