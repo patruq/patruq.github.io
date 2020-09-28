b77ed7e74401970e15129fc85d23e2cad898f1aa
patruq <pattymac.pm@gmail.com>
2020-09-28 14:08:53 -0400

    finished content about page

--- personal-website/src/components/routes/about.js
+++ personal-website/src/components/routes/about.js
@@ -15,7 +15,30 @@ export const About = () => (
             <p className="about-statement">Data and technology are changing how companies do business, and in turn changing the ways we live as residents, citizens, and consumers. <br/><br/>My core professional philosophy is to achieve goals using eagerness, cooperation, and humility. <br/><br/>I believe in taking an analytical approach <br/>towards the solving the problems facing our communities.</p>
         </div>
         <div className="bottom-section">
-            <br/>
+            <div className="left">
+                <h2 className="tech-types">Web</h2>
+                <ul>
+                    <li><img className="img-logo" src={require("../../assets/images/javascript-logo.png")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/react-logo.svg")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/npm-logo-red.svg")} /></li>
+                </ul>
+            </div>
+            <div className="center">
+                <h2 className="tech-types">Programming</h2>
+                <ul>
+                    <li><img className="img-logo" src={require("../../assets/images/python-logo-generic.svg")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/pandas-logo.svg")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/scikit-learn-logo-notext.png")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/logo-mysql-170x115.png")} /></li>
+                </ul>
+            </div>
+            <div className="right">
+                <h2 className="tech-types">Technical</h2>
+                <ul>
+                    <li><img className="img-logo" src={require("../../assets/images/git-Icon-Black.png")} /></li>
+                    <li><img className="img-logo" src={require("../../assets/images/bash-logo.png")} /></li>
+                </ul>
+            </div>
         </div>
     </body>
 )
